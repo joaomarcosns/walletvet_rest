9738b5a5ff3899d3a950a0bcc185a7d1757e753a
João Marcos Neves da Silva <joaomarcosnina@gmail.com>
2022-07-08 17:57:16 -0300

    refactor: Add birth date in model User

--- walletvet/users/models.py
+++ walletvet/users/models.py
@@ -1,12 +1,14 @@
+from pyexpat import model
 from django.contrib.auth.models import AbstractBaseUser
 from django.db import models
 
 # OBS: This is the user/owner model
 
 class User(AbstractBaseUser):
-    name = models.CharField('Name', max_length=100, null=False, blank=False)
+    name = models.CharField('Name', max_length=100)
     email = models.EmailField('Email', max_length=100, unique=True)
     is_active = models.BooleanField('active', default=True)
+    birth_date = models.DateField("birth date", auto_now_add=False)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
