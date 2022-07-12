583d9d01807152c837946cdeb357e2f451550af7
João Marcos Neves da Silva <joaomarcosnina@gmail.com>
2022-07-12 13:04:02 -0300

    feat: Added new attribute on user to have access to admin page

--- walletvet/users/models.py
+++ walletvet/users/models.py
@@ -23,9 +23,12 @@ class CustomUserManager(BaseUserManager):
 
     def create_superuser(self, email, password=None, **extra_fields):
         """Create and save a SuperUser with the given email and password."""
+        extra_fields.setdefault("is_staff", True)
         extra_fields.setdefault("is_superuser", True)
         extra_fields.setdefault("is_active", True)
 
+        if extra_fields.get("is_staff") is not True:
+            raise ValueError("Superuser must have is_staff=True.")
         if extra_fields.get("is_superuser") is not True:
             raise ValueError("Superuser must have is_superuser=True.")
         return self.create_user(email, password, **extra_fields)
@@ -45,6 +48,7 @@ class User(AbstractBaseUser, PermissionsMixin):
     email = models.EmailField('Email', max_length=100, unique=True)
     is_active = models.BooleanField('active', default=True)
     birth_date = models.DateField("birth date", auto_now_add=False)
+    is_staff = models.BooleanField('is_staff', default=False)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
