30b79f11b9864dc87c12246a80d552fea707a0f6
João Marcos Neves da Silva <joaomarcosnina@gmail.com>
2022-07-13 17:14:01 -0300

    refactor: Remove Color, HairType

--- animal/models.py
+++ animal/models.py
@@ -1,18 +1,6 @@
 from django.db import models
 from django.utils.translation import gettext_lazy as _
 
-
-class Color(models.Model):
-    color = models.CharField(_('Color'), max_length=150)
-
-    class Meta:
-        db_table='color'
-
-class HairType(models.Model):
-    hair_type = models.CharField(_("Hair Type"), max_length=100)
-    class Meta:
-        db_table='hair_type'
-
 class TypeAnimal(models.Model):
     type_animal = models.CharField(_("Type Animal"), max_length=100)
     class Meta:
@@ -20,4 +8,7 @@ class TypeAnimal(models.Model):
 
 class Breed(models.Model):
     breed = models.CharField(_("Breed"), max_length=200)
-    fk_type_animal = models.ForeignKey(_("Type Animal"))
+    fk_type_animal = models.ForeignKey(_("Type Animal"), TypeAnimal)
+    class Meta:
+        db_table='breed'
+
