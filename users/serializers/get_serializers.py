a696097a08659440c3a9f63e87467c986a7ecc06
João Marcos Neves da Silva <joaomarcosnina@gmail.com>
2022-07-13 11:57:58 -0300

    refactor: Refactor business roles

--- users/serializers/get_serializers.py
+++ users/serializers/get_serializers.py
@@ -10,6 +10,6 @@ from rest_framework.response import Response
 class UserListSerializers(serializers.ModelSerializer):
     class Meta:
         model = User
-        exclude = ['password', 'user_permissions', 'groups', 'is_active', "is_staff"]
+        exclude = ['password', 'user_permissions', 'groups', 'is_superuser', "is_staff"]
 
        
