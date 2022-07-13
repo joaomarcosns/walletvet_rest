588eb7ac2b42d2a47193dd3da03e0bc9571ad0e8
João Marcos Neves da Silva <joaomarcosnina@gmail.com>
2022-07-13 18:06:01 -0300

    feat: Add class Animal in app Animal

--- animal/models.py
+++ animal/models.py
@@ -1,5 +1,12 @@
+from users.models import User
 from django.db import models
 from django.utils.translation import gettext_lazy as _
+
+class Color(models.Model):
+    color = models.CharField(_('Color'), max_length=150)
+
+    class Meta:
+        db_table='color'
 
 class TypeAnimal(models.Model):
     type_animal = models.CharField(_("Type Animal"), max_length=100)
@@ -8,7 +15,29 @@ class TypeAnimal(models.Model):
 
 class Breed(models.Model):
     breed = models.CharField(_("Breed"), max_length=200)
-    fk_type_animal = models.ForeignKey(_("Type Animal"), TypeAnimal)
+    fk_type_animal = models.ForeignKey(TypeAnimal, on_delete=models.CASCADE)
     class Meta:
         db_table='breed'
 
+class Animal(models.Model):
+    GENDER_CHOICES = (
+        ("F", "Female"),
+        ("M", "Male")
+    )
+    HAIR_TYPE_CHOICES = (
+        ("S", "Small"),
+        ("M", "Mid"),
+        ("L", "Large")
+    )
+    name = models.CharField(_('Name'), max_length=100)
+    birth_date = models.DateField(_('birth date'), auto_now_add=False)
+    gender = models.CharField(_("Gender"), max_length=1, choices=GENDER_CHOICES, blank=False, null=False)
+    description = models.TextField(_('Description'), max_length=999)
+    photo = models.ImageField(_('photo'))
+    hair_type = models.CharField(_("hair Type"), max_length=1, choices=HAIR_TYPE_CHOICES, blank=False, null=False)
+    fk_breed = models.ForeignKey(Breed, on_delete=models.CASCADE)
+    fk_color = models.ForeignKey(Color, on_delete=models.CASCADE)
+    fk_type_animal = models.ForeignKey(TypeAnimal, on_delete=models.CASCADE)
+    fk_user = models.ForeignKey(User, on_delete=models.CASCADE)
+    class Meta:
+            db_table='animal'
