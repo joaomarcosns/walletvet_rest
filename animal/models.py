0eecbca6764d6b0b43d368181bd69a647b8eb4bf
João Marcos Neves da Silva <joaomarcosnina@gmail.com>
2022-07-13 16:44:48 -0300

    feat: Add class hair_type in app Animal

--- animal/models.py
+++ animal/models.py
@@ -5,3 +5,11 @@ from django.utils.translation import gettext_lazy as _
 
 class Color(models.Model):
     color = models.CharField(_('Color'), max_length=150)
+
+    class Meta:
+        db_table='color'
+
+class HairType(models.Model):
+    hair_type = models.CharField(_("Hair Type"), max_length=100)
+    class Meta:
+        db_table='hair_type'
