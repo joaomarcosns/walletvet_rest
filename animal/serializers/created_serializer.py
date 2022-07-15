e1c9e9b5151c9d6640165c303549846a0fc3ac32
João Marcos Neves da Silva <joaomarcosnina@gmail.com>
2022-07-14 22:34:26 -0300

    feat: returns 400 if date of birth is less than one day

--- animal/serializers/created_serializer.py
+++ animal/serializers/created_serializer.py
@@ -4,6 +4,8 @@ from animal.models import (
 )
 # Framework
 from rest_framework import serializers
+from django.utils.timezone import now
+from dateutil.relativedelta import relativedelta
 from django.utils.translation import gettext_lazy as _
 
 class AnimalCreatedSerializer(serializers.ModelSerializer):
@@ -14,6 +16,12 @@ class AnimalCreatedSerializer(serializers.ModelSerializer):
     def validate_name(self, value):
         if any(chr.isdigit() for chr in value):
             raise serializers.ValidationError(_("Invalid name entered"), "name_invalid")
+        return value
+
+    def validate_birth_date(self, value):
+        difference = relativedelta(now().date(),value)
+        if difference.days < 0:
+            raise serializers.ValidationError(_("User must be over 18 years old"), "birth_date_invalid")
         return value
 
     def create(self, validated_data):
