a70cc28e98ffd4ed21992f3389a58c734f650edc
João Marcos Neves da Silva <joaomarcosnina@gmail.com>
2022-07-15 18:09:29 -0300

    feat: Add admin all models

--- animal/admin.py
+++ animal/admin.py
@@ -1,3 +1,29 @@
+from codecs import getencoder
 from django.contrib import admin
-
+from .models import (
+   Animal,
+   Breed,
+   Color,
+   TypeAnimal, 
+)
 # Register your models here.
+
+class AnimalShowAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'birth_date')
+    list_filter =  ('id', 'name', 'birth_date')
+
+class BreedShowAdmin(admin.ModelAdmin):
+    list_display = ('id', 'breed', 'fk_type_animal')
+    list_filter =  ('fk_type_animal', )
+
+class ColorShowAdmin(admin.ModelAdmin):
+    list_display = ('color',)
+
+class TypeAnimalShowAdmin(admin.ModelAdmin):
+    list_display = ('type_animal',)
+
+admin.site.register(Animal, AnimalShowAdmin)
+admin.site.register(Breed, BreedShowAdmin)
+admin.site.register(Color, ColorShowAdmin)
+admin.site.register(TypeAnimal, TypeAnimalShowAdmin)
+
--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from users.models import User
 # Register your models here.
+
+class UserShowAdmin(admin.ModelAdmin):
+    list_display = ('id', 'email', 'name')
+    list_filter =  ('id', 'birth_date', 'is_active')
+
+admin.site.register(User, UserShowAdmin)
