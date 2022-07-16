d6cca9706300d225a008ecbf9891f0289cfecfba
João Marcos Neves da Silva <joaomarcosnina@gmail.com>
2022-07-16 17:41:53 -0300

    feat: returns 400 if the vaccination date is greater than the current one

--- vaccination/serializers/create_serializers.py
+++ vaccination/serializers/create_serializers.py
@@ -1,6 +1,8 @@
 # App
 from vaccination.models import Vaccination
 # Framework
+from django.utils.timezone import now
+from dateutil.relativedelta import relativedelta
 from rest_framework import serializers
 from django.utils.translation import gettext_lazy as _
 
@@ -15,6 +17,11 @@ class VaccinationCreateSerializers(serializers.ModelSerializer):
         if any(chr.isdigit() for chr in value):
             raise serializers.ValidationError(_("Invalid responsible entered"), "responsible_invalid")
         return value
+
+    def validate_date_vaccination(self, attrs):
+        if attrs > now():
+            raise serializers.ValidationError(_("More than current data"), "date_vaccination_invalid")
+        return super().validate(attrs)
     
     def create(self, validated_data):
         vaccination = Vaccination.objects.create(**validated_data)
